715ddf7db3df0a177c40e13bb8370820e7d2d2d3
emAmazed <emilieying@yahoo.com>
2013-01-16 12:18:53 -0500

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foo1::Application.config.secret_token = '9649840aac15eed185388271904269851a7a0b2e5abee2fd4a96c5b21c6da9ec4e5371f01c76e325a9ece9633716bbd6557944803dbcf7ef9b8db084686345b2'
